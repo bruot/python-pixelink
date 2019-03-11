ceecb4411a1504c7a47ebbac06563429f544ebc1
Nicolas Bruot <coremeltdown@bruot.org>
2019-03-11 15:07:59 +0100

    Adding GPL licence

--- pixelink/example.py
+++ pixelink/example.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Tools for Pixelink PDS movies
 
-@author: Nicolas Bruot (https://www.bruot.org/hp/)
-@date: 29 May 2018
-"""
+# This file is part of the python-pixelink software.
+#
+# Copyright 2018-2019 Nicolas Bruot and CNRS (https://www.bruot.org/hp/)
+#
+#
+# python-pixelink is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# python-pixelink is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with python-pixelink.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""Example showing how to use python-pixelink"""
+
 
 from matplotlib import pyplot as plt
 import numpy as np
